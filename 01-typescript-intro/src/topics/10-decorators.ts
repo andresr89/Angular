9347279fce537efd62820e6ca9c1b0d6aff0ba55
Andres Rodriguez <andresarodriguezr89@gmail.com>
2023-12-14 20:39:56 -0500

    generics and decorators

---
+++ 01-typescript-intro/src/topics/10-decorators.ts
@@ -0,0 +1,23 @@
+function classDecorator (){
+    
+}
+
+
+
+
+
+export class SuperClass {
+
+    public myProperty: string = 'Abc123';
+
+    print (){
+        console.log ('Hola Mundo')
+    }
+
+}
+
+console.log (SuperClass)
+
+const myClass = new SuperClass;
+console.log (myClass);
+ 
