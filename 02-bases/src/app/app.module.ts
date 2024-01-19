d4bd7bd9b4a00d82df0a0f86103c0b78d1fa0339
Andres Rodriguez <andresarodriguezr89@gmail.com>
2024-01-18 19:45:14 -0500

    components and modules

--- 02-bases/src/app/app.module.ts
+++ 02-bases/src/app/app.module.ts
@@ -2,19 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
+
 import { CounterModule } from './counter/components/counter/counter.module';
+import { heroesModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroComponent,
-    ListComponent
+
   ],
   imports: [
     BrowserModule,
     CounterModule,
+    heroesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
