7292e0a40875550db18d283a73cea14b38169ea9
Andres Rodriguez <andresarodriguezr89@gmail.com>
2024-01-20 14:53:09 -0500

    dbz-list-component

--- 02-bases/src/app/dbz/dbz.module.ts
+++ 02-bases/src/app/dbz/dbz.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page.component';
+import { ListComponent } from './components/list/list.component';
 
 
 @NgModule({
   declarations: [
-    MainPageComponent
+    MainPageComponent,
+    ListComponent
   ],
 
   exports:[
-    MainPageComponent
+    MainPageComponent,
+
   ],
 
   imports: [
