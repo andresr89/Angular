77e8312280888c3f7d0db68fe2bc7abf40aa2aad
Andres Rodriguez <andresarodriguezr89@gmail.com>
2024-02-05 17:12:08 -0500

    @input

--- 02-bases/src/app/dbz/components/list/list.component.ts
+++ 02-bases/src/app/dbz/components/list/list.component.ts
@@ -1,8 +1,16 @@
- import { ChangeDetectionStrategy, Component } from '@angular/core';
+ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-list',
   templateUrl:'./list.component.html' ,
   styleUrls: ['./list.component.css'],
 })
-export class ListComponent { }
+export class ListComponent {
+
+  @Input()
+  public characterList : Character [] = [{
+    name : 'Trunks',
+    power : 10
+  }]
+ }
